bab2ba8b1c9c0a81ac21bd5b2e5b746fddedc66b
Max Bakulin <mbakulin102@gmail.com>
2022-09-16 21:07:58 +0300

    first commit

---
+++ Lesson1/Задача 2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int FirstUserNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число (отличное от первого): ");
+int SecondUserNumber = Convert.ToInt32(Console.ReadLine());
+if (FirstUserNumber == SecondUserNumber)
+{
+    Console.WriteLine("Вы ввели два одинаковых числа!");
+    return;
+}
+if ((FirstUserNumber) > (SecondUserNumber))
+{
+    Console.WriteLine($"Первое число {FirstUserNumber} больше второго числа {SecondUserNumber}");
+}
+else
+{
+    Console.WriteLine($"Второе число {SecondUserNumber} больше первого числа {FirstUserNumber}");
+}
