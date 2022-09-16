0810dd237b0f4306d15439470d23675c1a8f237a
Max Bakulin <mbakulin102@gmail.com>
2022-09-16 21:28:44 +0300

    third commit

---
+++ Lesson1/Задача 6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+if (UserNumber %2 == 0)
+{
+    Console.WriteLine($"Число {UserNumber} четное");
+}
+else
+{
+    Console.WriteLine($"Число {UserNumber} нечетное");
+}
