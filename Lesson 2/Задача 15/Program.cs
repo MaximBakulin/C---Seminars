1d8cfa74926a29d61c8e8f6d1f50e46a56c4f422
Max Bakulin <mbakulin102@gmail.com>
2022-09-23 18:25:39 +0300

    lesson 2 commit 6

---
+++ Lesson 2/Задача 15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7: ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+int getChekNumberRange(int number)
+{
+    if(number > 7 || number < 1)
+    {
+        Console.WriteLine("Вы ввели цифру вне указанного диапазона!");
+        Environment.Exit(0);
+    }
+    return number;
+}
+getChekNumberRange(UserNumber);
+if(UserNumber > 5)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
