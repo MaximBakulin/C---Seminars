133a6f53c04a2b795dcb8971b65073b24b0d4133
Max Bakulin <mbakulin102@gmail.com>
2022-09-23 15:27:50 +0300

    lesson 2 commit 2

--- Lesson 2/Задача 10/Program.cs
+++ Lesson 2/Задача 10/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int getRandomNumberOfThreeDigit(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder + 1);
+    return result;
+}
+int getSecondDigitInRandomDigit(int digit)
+{
+    int secondDigit = digit / 10;
+    secondDigit = secondDigit % 10;
+    return secondDigit;
+}
+int randomDigit = getRandomNumberOfThreeDigit(100, 999);
+int secondNumber = getSecondDigitInRandomDigit(randomDigit);
+Console.WriteLine($"Случайное значение {randomDigit}");
+Console.WriteLine($"Вторая цифра {secondNumber}");
