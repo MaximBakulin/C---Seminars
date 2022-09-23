cf6a7bb49bba922324fe2d16a5dc286876386942
Max Bakulin <mbakulin102@gmail.com>
2022-09-23 17:46:26 +0300

    lesson 2 commit 4

---
+++ Lesson 2/Задача 13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число: ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+if(UserNumber < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    Environment.Exit(0);
+}
+int getReverseUserNumber(int number)
+{
+    string s = number.ToString();
+    char[] ar = s.ToCharArray();
+    Array.Reverse(ar);
+    s = new String(ar);
+    number = Convert.ToInt32(s);
+    return number;
+}
+int getThirdDigitOfUserNumber(int number)
+{
+    int thirdNumber = number / 100;
+    thirdNumber = thirdNumber % 10;
+    Console.WriteLine($"Третья цифра {thirdNumber}");
+    return thirdNumber;
+}
+int newNumber = getReverseUserNumber(UserNumber);
+getThirdDigitOfUserNumber(newNumber);
