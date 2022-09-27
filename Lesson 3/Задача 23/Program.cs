6573d4ca156dff6d01dba60e37bd2b4ebee204c5
Max Bakulin <mbakulin102@gmail.com>
2022-09-27 17:47:54 +0300

    lesson 3 commit 3

---
+++ Lesson 3/Задача 23/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите целое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int[] arrayBasedOnUserNumber = new int[userNumber+1];
+
+void cubeСalculation(int[] someArray)
+{
+  int countOne = 0;
+  int length = someArray.Length;
+  while (countOne <  length)
+  {
+    someArray[countOne] = Convert.ToInt32(Math.Pow(countOne, 3));
+    countOne++;
+  }
+}
+
+void resultOutput(int[] outputArray)
+{
+  int countTwo = outputArray.Length;
+  int index = 0;
+  while(index < countTwo)
+  {
+    Console.WriteLine($"куб числа {index} = {outputArray[index]}");
+    index++;
+  }
+} 
+
+cubeСalculation(arrayBasedOnUserNumber);
+resultOutput(arrayBasedOnUserNumber);
