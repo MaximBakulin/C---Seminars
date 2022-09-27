bd0b170e96e76e861072c7cb622f5a97bd01b4c6
Max Bakulin <mbakulin102@gmail.com>
2022-09-27 17:11:31 +0300

    lesson 3 commit 2

---
+++ Lesson 3/Задача 21/Program.cs
@@ -0,0 +1,21 @@
+﻿int Coordinate(string coordinatName, string pointName)
+{
+    Console.Write($"Введите координату {coordinatName} точки {pointName} (целое число): ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int coordinatOnePointA = Coordinate("x", "A");
+int coordinatTwoPointA = Coordinate("y", "A");
+int coordinatThreePointA = Coordinate("z", "A");
+int coordinatOnePointB = Coordinate("x", "B");
+int coordinatTwoPointB = Coordinate("y", "B");
+int coordinatThreePointB = Coordinate("z", "B");
+
+double distanceCalculation(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+}
+
+double distanceBetweenPoints = distanceCalculation(coordinatOnePointA, coordinatOnePointB, coordinatTwoPointA, coordinatTwoPointB, coordinatThreePointA, coordinatThreePointB);
+
+Console.WriteLine($"Расстояние между точками = {distanceBetweenPoints}");
