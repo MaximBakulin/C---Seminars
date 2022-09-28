91033cc4320d6956f7ce69dc38fec750e270c574
Max Bakulin <mbakulin102@gmail.com>
2022-09-28 07:48:46 +0300

    lesson 4 commit 1

---
+++ Lesson 4/Задача 25/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите целое число, которое необходимо возвести в степень: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите степень, в которое необходимо возвести число {userNumber}");
+int userDegree = Convert.ToInt32(Console.ReadLine());
+
+int getExponentiation(int number, int degree)
+{
+    int tempNumber = number;
+    for(int i = 1; i < degree; i++)
+    {
+        tempNumber = tempNumber * number;
+    }
+    return tempNumber;
+}
+
+if(userNumber == 0 || userDegree == 0)
+{
+    Console.WriteLine($"Число {userNumber} в степени {userDegree} = 1");
+    Environment.Exit(0);
+}
+
+int result = getExponentiation(userNumber, userDegree);
+Console.WriteLine($"Число {userNumber} в степени {userDegree} = {result}");
