b8b7fe3aa861dc5385cef5942feb0e2e9ff060de
Max Bakulin <mbakulin102@gmail.com>
2022-09-28 17:46:24 +0300

    lesson 4 commit 3

---
+++ Lesson 4/Задача 29/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите длину массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arrayLength];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+}
+
+void OutputArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+OutputArray(array);
