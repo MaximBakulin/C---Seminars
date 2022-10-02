a457b76fad261cc565272ed25ace4699aa7fff53
Max Bakulin <mbakulin102@gmail.com>
2022-10-02 20:18:01 +0300

    lesson 5 commit 3

---
+++ Lesson 5/Задача 38/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine("Задайте длину массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] userArray = new int[arrayLength];
+
+for (int i = 0; i < userArray.Length; i++)
+{
+    userArray[i] = new Random().Next(1, 101);
+}
+
+void getPrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int getMaximumValueArray(int[] array)
+{
+    int maxValue = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxValue < array[i])
+        {
+            maxValue = array[i];
+        }
+    }
+    return maxValue;
+}
+
+int getMinimumValueArray(int[] array)
+{
+    int minValue = getMaximumValueArray(array);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (minValue > array[i])
+        {
+            minValue = array[i];
+        }
+    }
+    return minValue;
+}
+
+getPrintArray(userArray);
+int min = getMinimumValueArray(userArray);
+int max = getMaximumValueArray(userArray);
+Console.WriteLine($"Минимальное значение {min}");
+Console.WriteLine($"Максимальное значение {max}");
+int differenceMaxMinValue = getMaximumValueArray(userArray) - getMinimumValueArray(userArray);
+Console.WriteLine($"Разница между максимальным и минимальным значением массива = {differenceMaxMinValue}");
