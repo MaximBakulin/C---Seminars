c496f1b99dc5621ee4a654a407509293f21e43fc
Max Bakulin <mbakulin102@gmail.com>
2022-10-17 11:51:33 +0300

    lesson 9 commit 4

--- Lesson 9/Задача 68/Program.cs
+++ Lesson 9/Задача 68/Program.cs
@@ -1,18 +1,18 @@
 ﻿int userNumberOne = ConvertToInt("Введите первое значение: ");
 int userNumberTwo = ConvertToInt("Введите второе значение: ");
 
-int res = AckermanFunction(userNumberOne, userNumberTwo);
-Console.WriteLine(res);
+int result = AckermanFunction(userNumberOne, userNumberTwo);
+Console.WriteLine(result);
 
-int AckermanFunction(int n, int m)
+int AckermanFunction(int num1, int num2)
 {
-  if (n == 0)
-    return m + 1;
+  if (num1 == 0)
+    return num2 + 1;
   else
-    if ((n != 0) && (m == 0))
-      return AckermanFunction(n - 1, 1);
+    if ((num1 != 0) && (num2 == 0))
+      return AckermanFunction(num1 - 1, 1);
     else
-      return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+      return AckermanFunction(num1 - 1, AckermanFunction(num1, num2 - 1));
 }
 
 int ConvertToInt(string input)
