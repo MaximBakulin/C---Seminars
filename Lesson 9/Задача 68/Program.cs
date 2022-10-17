7141a3f79bbd2bd47090294d3c9d9484ff417ac6
Max Bakulin <mbakulin102@gmail.com>
2022-10-17 11:43:35 +0300

    lesson 9 commit 3

---
+++ Lesson 9/Задача 68/Program.cs
@@ -0,0 +1,23 @@
+﻿int userNumberOne = ConvertToInt("Введите первое значение: ");
+int userNumberTwo = ConvertToInt("Введите второе значение: ");
+
+int res = AckermanFunction(userNumberOne, userNumberTwo);
+Console.WriteLine(res);
+
+int AckermanFunction(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return AckermanFunction(n - 1, 1);
+    else
+      return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+}
+
+int ConvertToInt(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
