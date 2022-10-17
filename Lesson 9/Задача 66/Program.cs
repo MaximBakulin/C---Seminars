728d3b4ef6458cfeb41c9efd2f1eed7e4ab4a953
Max Bakulin <mbakulin102@gmail.com>
2022-10-17 11:04:58 +0300

    lesson 9 commit 2

---
+++ Lesson 9/Задача 66/Program.cs
@@ -0,0 +1,30 @@
+﻿int userNumberOne = ConvertToInt("Введите первое значение: ");
+int userNumberTwo = ConvertToInt("Введите второе значение: ");
+
+if (userNumberTwo < userNumberOne)
+{
+    Console.WriteLine("Второе число не может быть меньше первого!");
+    Environment.Exit(0);
+}
+
+int userResult = SumAllNumbersInRange(userNumberOne, userNumberTwo);
+Console.WriteLine(userResult);
+
+int SumAllNumbersInRange(int numberOne, int numberTwo)
+{
+    if (numberOne > numberTwo)
+    {
+        return 0;
+    }
+    else
+    {
+    return numberOne + SumAllNumbersInRange(numberOne + 1, numberTwo);
+    }
+}
+
+int ConvertToInt(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
